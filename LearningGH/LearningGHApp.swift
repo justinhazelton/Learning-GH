afef39c23b7786e5308a987de95ef0a687b56fd1
Justin Hazelton <justin@justinhazelton.com.au>
2021-10-02 09:31:40 +1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Justin Hazelton on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
